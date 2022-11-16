25ac127f7f427f07e1728cf363d7a99ea5402b8f
DeEUS-22 <gowell_kim@naver.com>
2022-11-16 17:13:29 +0900

    22.11.16
    1. 회원가입 db연동 관련 로직 完
    
    2. 로그인 db연동 관련 로직 작성 필요.

--- model1_cgv/src/com/model1_cgv/dao/CgvMemberDAO.java
+++ model1_cgv/src/com/model1_cgv/dao/CgvMemberDAO.java
@@ -1,5 +1,39 @@
 package com.model1_cgv.dao;
 
-public class CgvMemberDAO {
+import com.model1_cgv.vo.CgvMemberVO;
 
+public class CgvMemberDAO extends DBConn{
+	/**
+	 * insert : 회원가입 
+	 */
+	public int insert(CgvMemberVO vo) {
+		int result = 0;
+		String sql = "insert into cgv_member "
+				+ " values(?,?,?,?,?,?,?,?,?,?,?,?,sysdate)";
+		
+		try {
+			getPreparedStatement(sql);
+			pstmt.setString(1, vo.getId());
+			pstmt.setString(2, vo.getPass());
+			pstmt.setString(3, vo.getName());
+			pstmt.setString(4, vo.getGender());
+			pstmt.setString(5, vo.getEmail());
+			pstmt.setString(6, vo.getZonecode());
+			pstmt.setString(7, vo.getAddr1());
+			pstmt.setString(8, vo.getAddr2());
+			pstmt.setString(9, vo.getHp());
+			pstmt.setString(10, vo.getPnumber());
+			pstmt.setString(11, vo.getHobbylist());
+			pstmt.setString(12, vo.getIntro());
+			
+			result = pstmt.executeUpdate();
+			
+			close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
 }
